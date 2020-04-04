6a57434522f497051528cf4dca864616f6cfabd8
Nathan Bliss <ndb@software-automation.com>
2020-04-04 23:43:47 +0100

    Fix

--- ssh_script.sh
+++ ssh_script.sh
@@ -12,7 +12,7 @@ zip /tmp/postgres/export.csv.zip /tmp/postgres/export.csv
 scp -i ./postgres_node_private_key /tmp/postgres/export.csv.zip $1:/tmp/postgres
 scp -i ./postgres_node_private_key ./generate_import_sql.sh $1:/tmp/postgres
 
-ssh -i ./postgres_node_private_key $1 unzip-p /tmp/postgres/export.csv.zip
+ssh -i ./postgres_node_private_key $1 unzip /tmp/postgres/export.csv.zip
 
 ssh -i ./postgres_node_private_key $1 PGSOURCEIP=$PGTARGETIP \
 PGPASSWORD=$PGTARGETPASSWORD \
