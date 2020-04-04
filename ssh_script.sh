b402a69f7219f73afc44a1fbe4e88a16197bf35a
Nathan Bliss <ndb@software-automation.com>
2020-04-04 23:31:18 +0100

    Add zip capability

--- ssh_script.sh
+++ ssh_script.sh
@@ -6,15 +6,13 @@ mkdir -p /tmp/postgres
 ssh -i ./postgres_node_private_key $1 mkdir -p /tmp/postgres
 
 ./generate_export_sql.sh $2 $3
-scp -i ./postgres_node_private_key /tmp/postgres/export.csv $1:/tmp/postgres
+
+zip /tmp/postgres/export.csv.zip /tmp/postgres/export.csv
+
+scp -i ./postgres_node_private_key /tmp/postgres/export.csv.zip $1:/tmp/postgres
 scp -i ./postgres_node_private_key ./generate_import_sql.sh $1:/tmp/postgres
 
-ssh -i ./postgres_node_private_key $1 export PGTARGETIP=$PGTARGETIP
-ssh -i ./postgres_node_private_key $1 export PGPASSWORD=$PGPASSWORD
-ssh -i ./postgres_node_private_key $1 export PGUSERID=$PGUSERID
-
-ssh -i ./postgres_node_private_key $1 echo $PGSOURCEIP > /tmp/postgres/source_ip.txt
-ssh -i ./postgres_node_private_key $1 echo $PGTARGETIP > /tmp/postgres/target_ip.txt
+ssh -i ./postgres_node_private_key $1 unzip-p /tmp/postgres/export.csv.zip
 
 ssh -i ./postgres_node_private_key $1 PGSOURCEIP=$PGTARGETIP \
 PGPASSWORD=$PGTARGETPASSWORD \
