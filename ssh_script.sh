8247ac5f1638a7c5d8084285f11989786fd44156
Nathan Bliss <blissnd@hotmail.com>
2020-04-05 00:16:33 +0100

    Bugfixes

--- ssh_script.sh
+++ ssh_script.sh
@@ -5,14 +5,19 @@
 mkdir -p /tmp/postgres
 ssh -i ./postgres_node_private_key $1 mkdir -p /tmp/postgres
 
+rm /tmp/postgres/export.csv
+
 ./generate_export_sql.sh $2 $3
 
-zip /tmp/postgres/export.csv.zip /tmp/postgres/export.csv
+zip -j /tmp/postgres/export.csv.zip /tmp/postgres/export.csv
+
+ssh -i ./postgres_node_private_key $1 rm /tmp/postgres/export.csv
+ssh -i ./postgres_node_private_key $1 rm /tmp/postgres/export.csv.zip
 
 scp -i ./postgres_node_private_key /tmp/postgres/export.csv.zip $1:/tmp/postgres
 scp -i ./postgres_node_private_key ./generate_import_sql.sh $1:/tmp/postgres
 
-ssh -i ./postgres_node_private_key $1 unzip /tmp/postgres/export.csv.zip
+ssh -i ./postgres_node_private_key $1 unzip /tmp/postgres/export.csv.zip -d /tmp/postgres/
 
 ssh -i ./postgres_node_private_key $1 PGSOURCEIP=$PGTARGETIP \
 PGPASSWORD=$PGTARGETPASSWORD \
